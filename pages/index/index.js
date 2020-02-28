f841cab53bb95a22beb003f20a2ecc1377df0c51
LFH <LFH_chunshou@163.com>
2020-02-28 21:57:25 +0800

    导航栏完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,11 @@ import requset from '../../utils/request.js'
 
 Page({
   data: {
-    banners: []
+    banners: [],
+    menus: []
   },
   onLoad() {
+    //轮播图请求
     requset({
       //请求图片
       url: "/home/swiperdata"
@@ -19,7 +21,30 @@ Page({
       this.setData({
         banners: message
       })
-      console.log(message)
+      // console.log(message)
+    })
+    //导航栏的请求
+    requset({
+      url: "/home/catitems"
+    }).then(res => {
+      // console.log(res)
+      const {
+        message
+      } = res.data
+      // console.log(message)
+      const newData = message.map((v, i) => {
+        // console.log(v)
+        //代表分类
+        if (i === 0) {
+          v.url = "/pages/category/index"
+        }
+        return v
+      })
+      console.log(newData)
+      this.setData({
+        menus: newData
+      })
+
     })
   }
 
