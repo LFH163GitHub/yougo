bb345b8a2107d33b77ce6effe796fc0ebb976953
LFH <LFH_chunshou@163.com>
2020-03-06 19:27:59 +0800

    购物车的静态布局以及获取收获地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 收货地址
+    address: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //获取收获地址
+  handleGetAddress() {
+    wx.chooseAddress({
+      success: (res) => {
+        // 把收货地址保存到data
+        this.setData({
+          address: {
+            // 收货人
+            name: res.userName,
+            // 手机号码
+            tel: res.telNumber,
+            // 详细地址
+            detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+        console.log(res)
+      }
+    })
   }
 })
